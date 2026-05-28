e4f812b0e210102709f3b1cdbeb2a6aa1d5f3d85
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-05-28 11:42:56 +0000

    IP2BIN

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-IFS='.' read -r a b c d <<< "$1"
-printf '%08d.%08d.%08d.%08d\n' $(echo "obase=2;$a" | bc) $(echo "obase=2;$b" | bc) $(echo "obase=2;$c" | bc) $(echo "obase=2;$d" | bc)
+echo "$1" | tr '.' '\n' | while read o; do printf '%08d.' $(echo "obase=2;$o" | bc); done | sed 's/\.$//'; echo
