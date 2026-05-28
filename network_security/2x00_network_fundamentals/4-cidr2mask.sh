8bf36e74847b6a8e67b65e868e092de6f295a5e4
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-05-28 11:52:11 +0000

    CIDR TO MASK

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$(( 0xFFFFFFFF << (32-$1) & 0xFFFFFFFF )); printf "%d.%d.%d.%d\n" "$(( (mask>>24)&255 ))" "$(( (mask>>16)&255 ))" "$(( (mask>>8)&255 ))" "$(( mask&255 ))"
+mask=$(( 0xFFFFFFFF << (32-$1) & 0xFFFFFFFF )); printf "%d.%d.%d.%d" "$(( (mask>>24)&255 ))" "$(( (mask>>16)&255 ))" "$(( (mask>>8)&255 ))" "$(( mask&255 ))"
